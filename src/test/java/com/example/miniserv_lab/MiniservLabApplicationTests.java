bd5d3f517922a97ecc33b49fec61e6e1c4a62240
VadimKost <39429087+VadimKost@users.noreply.github.com>
2022-10-17 22:58:43 +0300

    init

---
+++ src/test/java/com/example/miniserv_lab/MiniservLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniserv_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniservLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
